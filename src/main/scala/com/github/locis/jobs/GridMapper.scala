a411edd48a95dbef02853c84e9f96d17270af791
shagun Sodhani <sshagunsodhani@gmail.com>
2016-04-16 16:22:28 +0530

    Added dummy mapper to map data points to grid number

---
+++ src/main/scala/com/github/locis/jobs/GridMapper.scala
@@ -0,0 +1,23 @@
+package com.github.locis.jobs
+
+import org.apache.hadoop.io.LongWritable
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.mapreduce.Mapper
+
+/*
+ * This class maps the input data points to the grid number. For now, we are 
+ * using a dummy implementation. 
+ * See : https://github.com/shagunsodhani/locis/issues/2
+ */
+
+class GridMapper extends Mapper[LongWritable, Text, LongWritable, Text] {
+
+  private def getGridNumber(dataPoint: String): Long = {
+    1L
+  }
+  def map(key: LongWritable, value: Text, context: Context) = {
+    val gridNumber = new LongWritable(getGridNumber(value.toString()))
+    context.write(gridNumber, value)
+  }
+
+}
