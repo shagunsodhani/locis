205be77f021bd6e7ddc038d1c1c47239bb8786ad
shagun Sodhani <sshagunsodhani@gmail.com>
2016-05-07 16:15:19 +0530

    Completed Mapper for PatternSearch. Related to #7

--- src/main/scala/com/github/locis/map/PatternSearchMapper.scala
+++ src/main/scala/com/github/locis/map/PatternSearchMapper.scala
@@ -21,20 +21,10 @@ class PatternSearchMapper extends Mapper[LongWritable, Text, Text, Text] {
   private val hBaseUtil = new HBaseUtil()
 
   private val internalSeprator = "#"
-//  used to separate records within a colocation instance
-  
-  private val externalSeperator = "\t"
-//  used to separate records in a list of colocations
+  //  used to separate records within a colocation instance
 
-  private def isPrevalentType(neighbor: String, k: Int): Boolean = {
-    if (k == 1) {
-      true
-    } else {
-      //      val neighborType = neighbor
-      //      !hBaseUtil.readColocationStoreTable(neighbor).isEmpty()
-      true
-    }
-  }
+  private val externalSeperator = "\t"
+  //  used to separate records in a list of colocations
 
   private def scanNTransactions(neighborhood: Array[String], k: Int): Iterator[Array[String]] = {
     val key = neighborhood(0)
@@ -52,10 +42,11 @@ class PatternSearchMapper extends Mapper[LongWritable, Text, Text, Text] {
       val rowName = sortedInstance
         .map { dataPoint => DataParser.getType(dataPoint) }
         .mkString(internalSeprator)
+
       hBaseUtil
-        .readColocationStoreTable(rowName = rowName, size = k - 1)
+        .readColocationStoreRow(rowName = rowName, size = k - 1)
         .split(externalSeperator)
-        .contains(sortedInstance)
+        .contains(sortedInstance.mkString(internalSeprator))
     }
   }
 
@@ -68,16 +59,27 @@ class PatternSearchMapper extends Mapper[LongWritable, Text, Text, Text] {
     val k = context.getConfiguration.get("k").toInt
     val line = value.toString().split(externalSeperator)
     val keyString = line(0)
+
+    /*
+     *     I have a feeling that this could explode for very large datasets. It should not be brought in-memory.
+     *     But querying HBase everytime would be very slow. Given the number of steps this set would take to be created, I am not convinced if
+     *     filtering on basis of prevalent colocation types would indeed benefit the implementation.
+     */
+    val prevalentColocationTypes = hBaseUtil
+      .scanColocationStoreColumn("size", k - 1)
+      .flatMap(colocationType => colocationType.split(internalSeprator).toIterable)
+      .toSet
+
     val neighborhood = line
       .tail
-      .filter { neighbor => isPrevalentType(neighbor, k) }
+      .filter { neighbor => prevalentColocationTypes.contains(DataParser.getType(neighbor)) }
 
     scanNTransactions(neighborhood, k)
       .filter { checkCliqueness(_, k) }
       .foreach { instance =>
         {
           val eventSet = (eventTypeOf(instance) ++ Array(DataParser.getType(keyString)))
-            .sortBy { dataPoint => DataParser.getType(dataPoint) }
+            .sortBy { identity }
             .mkString(internalSeprator)
           val sortedInstance = (instance ++ Array(keyString))
             .sortBy { dataPoint => DataParser.getType(dataPoint) }
