6bd7f09ac2308bfdf295364b022bbdbeae797866
shagun Sodhani <sshagunsodhani@gmail.com>
2016-05-07 12:02:35 +0530

    Added method to check cliqueness using HBase

--- src/main/scala/com/github/locis/map/PatternSearchMapper.scala
+++ src/main/scala/com/github/locis/map/PatternSearchMapper.scala
@@ -5,7 +5,9 @@ import org.apache.hadoop.io.Text
 import org.apache.hadoop.mapreduce.Mapper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+
 import com.github.locis.utils.DataParser
+import com.github.locis.utils.HBaseUtil
 
 /*
  * This class maps the input data points to (eventset, instance).
@@ -16,13 +18,20 @@ class PatternSearchMapper extends Mapper[LongWritable, Text, Text, Text] {
 
   private val logger: Logger = LoggerFactory.getLogger(getClass)
 
-  private val sep = "#"
+  private val hBaseUtil = new HBaseUtil()
+
+  private val internalSeprator = "#"
+//  used to separate records within a colocation instance
+  
+  private val externalSeperator = "\t"
+//  used to separate records in a list of colocations
 
   private def isPrevalentType(neighbor: String, k: Int): Boolean = {
     if (k == 1) {
       true
     } else {
-      //     To be replaced by HBase logic
+      //      val neighborType = neighbor
+      //      !hBaseUtil.readColocationStoreTable(neighbor).isEmpty()
       true
     }
   }
@@ -30,16 +39,23 @@ class PatternSearchMapper extends Mapper[LongWritable, Text, Text, Text] {
   private def scanNTransactions(neighborhood: Array[String], k: Int): Iterator[Array[String]] = {
     val key = neighborhood(0)
     neighborhood
-    .filterNot { _.equals(key) }
-    .combinations(k - 1)
+      .filterNot { _.equals(key) }
+      .combinations(k - 1)
   }
 
-  private def checkCliqueness(instance: Array[String]): Boolean = {
+  private def checkCliqueness(instance: Array[String], k: Int): Boolean = {
     if (instance.isEmpty) {
       true
     } else {
-      //      To be replaced by HBase logic
-      true
+      val sortedInstance = instance
+        .sortBy { dataPoint => DataParser.getType(dataPoint) }
+      val rowName = sortedInstance
+        .map { dataPoint => DataParser.getType(dataPoint) }
+        .mkString(internalSeprator)
+      hBaseUtil
+        .readColocationStoreTable(rowName = rowName, size = k - 1)
+        .split(externalSeperator)
+        .contains(sortedInstance)
     }
   }
 
@@ -50,23 +66,23 @@ class PatternSearchMapper extends Mapper[LongWritable, Text, Text, Text] {
   override def map(key: LongWritable, value: Text,
                    context: Mapper[LongWritable, Text, Text, Text]#Context) = {
     val k = context.getConfiguration.get("k").toInt
-    val line = value.toString().split("\t")
+    val line = value.toString().split(externalSeperator)
     val keyString = line(0)
     val neighborhood = line
       .tail
       .filter { neighbor => isPrevalentType(neighbor, k) }
 
     scanNTransactions(neighborhood, k)
-      .filter { checkCliqueness }
+      .filter { checkCliqueness(_, k) }
       .foreach { instance =>
         {
           val eventSet = (eventTypeOf(instance) ++ Array(DataParser.getType(keyString)))
             .sortBy { dataPoint => DataParser.getType(dataPoint) }
-            .mkString(sep)
+            .mkString(internalSeprator)
           val sortedInstance = (instance ++ Array(keyString))
             .sortBy { dataPoint => DataParser.getType(dataPoint) }
-            .mkString(sep)
-          context.write(new Text(eventSet), new Text(instance.mkString(sep)))
+            .mkString(internalSeprator)
+          context.write(new Text(eventSet), new Text(sortedInstance))
         }
       }
   }
